3f4e49b7bbee15e5572faebcec81cbb323e4c2cf
mi0706chi <mi0706chi@gmail.com>
2020-12-08 18:19:19 +0900

    Fix syntax around true

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,4 @@
 class Micropost < ApplicationRecord
 	belongs_to :user
-	validates :content,length: {minimum:1, maximum:140},presence :true
+	validates :content,length: {minimum:1, maximum:140},presence: true
 end
