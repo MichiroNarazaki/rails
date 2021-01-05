dd17cb037344257f38a3a2eb8e2ff3c2eff4ea54
mi0706chi <mi0706chi@gmail.com>
2021-01-05 19:26:37 +0900

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
+  has_many :microposts, dependent: :destroy #userが消えるとmicropostも消える
   before_save   :downcase_email
   before_create :create_activation_digest
   # before_save { self.email = email.downcase }
@@ -58,6 +59,12 @@ class User < ApplicationRecord
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
   # メールアドレスをすべて小文字にする
