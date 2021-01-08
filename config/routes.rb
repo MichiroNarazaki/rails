8f111cd5aeb4591a5c287549ca494f24ac5c73ab
mi0706chi <mi0706chi@gmail.com>
2021-01-09 00:24:57 +0900

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,12 @@ Rails.application.routes.draw do
   get '/about' ,to: 'static_pages#about'
   get '/contact' ,to: 'static_pages#contact'
   resources :microposts,          only: [:create, :destroy]
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships,       only: [:create, :destroy]
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
